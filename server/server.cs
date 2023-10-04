b0691640ea7cf55ada0ba621dfe2cb830ec2d70c
WORUS <aswoks@gmail.com>
2023-10-04 12:32:35 +0400

    Добавьте файлы проекта.

---
+++ server/server.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace server
+{
+    class server
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct DEVMODE
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmDeviceName;
+
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public int dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmFormName;
+
+            public short dmLogPixels;
+            public short dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        };
+
+        public class NativeMethods
+        {
+            // Affirms that the platform invoke
+            [DllImport("user32.dll")]
+            public static extern int EnumDisplaySettings(
+              string deviceName, int modeNum, ref DEVMODE devMode);
+            [DllImport("user32.dll")]
+            public static extern int ChangeDisplaySettings(
+                  ref DEVMODE devMode, int flags);
+
+            // Control to change the screen resolution.
+            public const int ENUM_CURRENT_SETTINGS = -1;
+            public const int CDS_UPDATEREGISTRY = 0x01;
+            public const int CDS_TEST = 0x02;
+            public const int DISP_CHANGE_SUCCESSFUL = 0;
+            public const int DISP_CHANGE_RESTART = 1;
+            public const int DISP_CHANGE_FAILED = -1;
+
+            // Constant definition control to change direction
+            public const int DMDO_DEFAULT = 0;
+            public const int DMDO_180 = 2;
+        }
+
+        private static void RotateWindow() {
+            DEVMODE devmode = new DEVMODE();
+            devmode.dmDeviceName = new String(new char[32]);
+            devmode.dmFormName = new String(new char[32]);
+            devmode.dmSize = (short)Marshal.SizeOf(devmode);
+
+            if (0 != NativeMethods.EnumDisplaySettings(null, NativeMethods.ENUM_CURRENT_SETTINGS, ref devmode))
+            {
+                devmode.dmDisplayOrientation = NativeMethods.DMDO_180;
+
+                // Change the screen resolution
+                int iRet = NativeMethods.ChangeDisplaySettings(ref devmode, NativeMethods.CDS_TEST);
+
+                if (iRet == NativeMethods.DISP_CHANGE_FAILED)
+                {
+                    Console.Write("Can't you request execution");
+                }
+                else
+                {
+                    iRet = NativeMethods.ChangeDisplaySettings(ref devmode, NativeMethods.CDS_UPDATEREGISTRY);
+
+                    switch (iRet)
+                    {
+                        // Successful change
+                        case NativeMethods.DISP_CHANGE_SUCCESSFUL:
+                            {
+                                break;
+                            }
+                        case NativeMethods.DISP_CHANGE_RESTART:
+                            {
+                                Console.Write("You need to restart your computer settings to take effect");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Write("Change the screen resolution failed");
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = IPAddress.Parse("192.168.1.105");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            string global_data = null;
+
+            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Начинаем слушать соединения
+                while (true)
+                {
+                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
+
+                    // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    string data = null;
+
+                    // Мы дождались клиента, пытающегося с нами соединиться
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    // Показываем данные на консоли
+                    Console.Write("Полученный текст: " + data + "\n\n");
+                    global_data = data;
+                    if (global_data == "1") RotateWindow();
+
+                    // Отправляем ответ клиенту\
+                    string reply = "Спасибо за запрос в " + data.Length.ToString()
+                            + " символов";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("Сервер завершил соединение с клиентом.");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+                
+
+            if (global_data == "1")
+            {
+            
+          
+        }
+
+
+            }
+
+
+        }
+
+
+    }
